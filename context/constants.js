21a78aab85ba1830d59b8e229d57416631ab82c2
golu91 <golu7903057@gmail.com>
2024-03-12 09:37:37 +0530

    done

---
+++ context/constants.js
@@ -0,0 +1,6 @@
+//organizerAddress = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
+
+import voting from "./Create.json";
+
+export const VotingAddress = "0xE23731f15F0887925903EE329ee3384EF73C74fc";//0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const VotingAddressABI = voting.abi;
